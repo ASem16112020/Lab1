3044503db2ae9811cbc13d4fc387236986329e1e
Asem <naasem2005@gmail.com>
2024-09-13 11:30:36 +0500

    first commit

--- src/main/java/org/example/laboratory1/Laboratory1Application.java
+++ src/main/java/org/example/laboratory1/Laboratory1Application.java
@@ -3,6 +3,7 @@ package org.example.laboratory1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -17,10 +18,18 @@ public class Laboratory1Application {
 
 
 	public class HelloWorld {
-		@GetMapping
+		/*@GetMapping
 		public String helloWorld() {
 			return "Hello World";
 		}
-
+		@GetMapping("/getId")
+		public String getId(@RequestParam String id) {
+			return "Hello World" + " " + id;
+		}*/
+		@GetMapping("/")
+		public String helloWorld(@RequestParam(name = "q", required = false, defaultValue = "0") String q) {
+			return "Hello World " + q;
+		}
 	}
+
 }
